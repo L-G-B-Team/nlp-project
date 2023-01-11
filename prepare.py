defe1eab54ec8eeeac0c2ae3903b40e756cef506
Woody Sims <sims.woodrow@gmail.com>
2023-01-11 14:16:43 -0600

    docstrings on prepare

--- prepare.py
+++ prepare.py
@@ -12,7 +12,13 @@ stopwords = stpwrds.words('english')
 
 
 def basic_clean(string: str) -> str:
-    # TODO Docstring
+    '''
+    Cleans string by converting to lower case and removing non-ACII characters
+    ## Parameters
+    string: string to be cleaned
+    ## Returns
+    cleaned string
+    '''
     string = string.lower()
     string = unicodedata.normalize('NFKD', string).encode(
         'ascii', 'ignore').decode('utf-8', 'ignore')
@@ -21,20 +27,38 @@ def basic_clean(string: str) -> str:
 
 
 def tokenize(string: str) -> str:
-    # TODO Docstring
+    '''
+    Tokenizes a given string
+    ## Parameters
+    string: string to be tokenized
+    ## Returns
+    tokenized string
+    '''
     tok = nltk.tokenize.ToktokTokenizer()
     return tok.tokenize(string, return_str=True)
 
 
 def stem(tokens: str) -> str:
-    # TODO Docstring
+    '''
+    Stems given string
+    ## Parameters
+    tokens: tokenized string to be stemmed
+    ## Returns
+    stemmed string
+    '''
     ps = nltk.porter.PorterStemmer()
     ret = [ps.stem(s) for s in tokens.split()]
     return ' '.join(ret)
 
 
 def lemmatize(tokens: str) -> str:
-    # TODO Docstring
+    '''
+    Lemmatizes given string
+    ## Parameters
+    tokens: tokenized string to be lemmatized
+    ## Returns
+    lemmatized string
+    '''
     lem = nltk.stem.WordNetLemmatizer()
     ret = [lem.lemmatize(s) for s in tokens.split()]
     return ' '.join(ret)
@@ -43,7 +67,17 @@ def lemmatize(tokens: str) -> str:
 def remove_stopwords(tokens: str,
                      extra_words: List[str] = [],
                      exclude_words: List[str] = []) -> str:
-    # TODO Docstring
+    '''
+    Removes stop words from string
+    ## Parameters
+    tokenized: initial string
+
+    extra_words: list of strings of additional stop words to remove
+
+    exclude_words: list of strings of stop words to keep in strings
+    ## Returns
+    string with stopwords removed
+    '''
     tokens = [t for t in tokens.split()]
     for exc in exclude_words:
         stopwords.remove(exc)
@@ -55,16 +89,43 @@ def remove_stopwords(tokens: str,
 
 def squeaky_clean(string: str, extra_words: List[str] = [],
                   exclude_words: List[str] = []) -> str:
+    '''
+    cleans, tokenizes, and removes stop words from string
+    ## Parameters
+    string: string to be cleaned
+
+    extra_words: list of strings of additional stop words to remove
+
+    exclude_words: list of strings of stop words to keep in strings
+    ## Returns
+    The cleaned string
+    '''
     string = basic_clean(string)
     string = tokenize(string)
     return remove_stopwords(string, extra_words, exclude_words)
 
 
-def prep_df_for_nlp(df: pd.DataFrame, ser: str,
+def prep_df_for_nlp(df: pd.DataFrame, series_to_prep: str,
                     extra_words: List[str] = [],
                     exclude_words: List[str] = []) -> pd.DataFrame:
-    df['clean'] = df[ser].apply(
+    '''
+    Cleans and prepares a `DataFrame` for NLP
+    ## Parameters
+        df: `DataFrame` to be cleaned
+
+        series_to_prep: name of the series to be prepared within `df`
+
+        extra_words: list of strings of additional stop words to remove
+
+        exclude_words: list of strings of stop words to keep in strings
+
+    ## Returns
+    Prepared `DataFrame`
+    '''
+    df['clean'] = df[series_to_prep].apply(
         squeaky_clean, exclude_words=exclude_words, extra_words=extra_words)
     df['stem'] = df['clean'].apply(stem)
     df['lemmatized'] = df['clean'].apply(lemmatize)
     return df
+
+
