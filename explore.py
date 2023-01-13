f61931f2950b6768b8f79b5cc0dcc4692d403468
Woody Sims <sims.woodrow@gmail.com>
2023-01-12 18:13:24 -0600

    changed heatmap to show something interesting

--- explore.py
+++ explore.py
@@ -131,8 +131,8 @@ def top_five_words(series: pd.Series) -> pd.DataFrame:
         readme_counts[f'count_{i}'] = readme_counts[f'count_{i}'
                                                     ].fillna(0).astype('int')
     return readme_counts
- 
-    
+
+
 def split_by_language(df):
     '''
     Takes in the dataframe and splits on the languages
@@ -145,27 +145,29 @@ def split_by_language(df):
     other = df[df.language == 'Other']
     python = df[df.language == 'Python']
     typescript = df[df.language == 'TypeScript']
-    
+
     return go, java, javascript, not_listed, other, python, typescript
 
-def top_words_by_group(df:pd.DataFrame,content:str='lemmatized',separator:str = 'language')->pd.DataFrame:
+
+def top_ngrams_by_group(df: pd.DataFrame, content: str = 'lemmatized',
+                        separator: str = 'language') -> pd.DataFrame:
     # TODO Docstring
-    total_items = df.shape[0]
     words = p.generate_series(df[content])
-    top_ten = pd.Series(words.split()).value_counts()[:10]
+    top_ten = pd.Series(words.split()).value_counts()
+    top_ten = top_ten[top_ten.index.str.len() > 3][:10]
     percentage_lst = []
     for s in df[separator].unique():
         ser = pd.Series(name=s)
+        separator_group = df[df[separator] == s]
         for w in top_ten.index:
-            containment_mask = (df[separator] == s) & (df[content].str.contains(w))
-            ser[w] = df[containment_mask].shape[0] / total_items
+            ser[w] = separator_group[
+                separator_group[
+                    content].str.contains(w)
+            ].shape[0] / separator_group.shape[0]
         percentage_lst.append(ser)
-    ret_frame =  pd.concat(percentage_lst,axis=1).T
-    total_values = df[separator].value_counts(normalize=True).T
-    total_values.name = 'All'
-    ret_frame = pd.concat([total_values,ret_frame],axis=1).T
-    return ret_frame
-        
-def word_heat_map(df:pd.DataFrame)->None:
+    return pd.concat(percentage_lst, axis=1)
+
+
+def word_heat_map(df: pd.DataFrame) -> None:
     # TODO Docstring
-    sns.heatmap(top_words_by_group(df))
+    sns.heatmap(top_ngrams_by_group(df))
