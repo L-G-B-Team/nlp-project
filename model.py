ba213a0574676d2bab17b8c1c7ed009e2e12853e
Woody Sims <sims.woodrow@gmail.com>
2023-01-13 14:32:32 -0600

    modeling docstrings

--- model.py
+++ model.py
@@ -31,7 +31,18 @@ def model_data(model: ModelType, features: pd.DataFrame, target: Union[pd.Series
 def tune_decision_tree(features: pd.DataFrame, target: pd.Series,
                        max_depth: Tuple[int, int, int] = (2, 31, 1),
                        metric: callable = accuracy_score):
-    # TODO
+    '''
+    
+    ## Parameters
+    features: `DataFrame` of features to model on
+    target: `Series` of target variable
+    max_depth: a tuple representing, in order, the minimum value for max_depth,
+    the maximum value for max_depth, and the number of steps between.
+    These function as parameters for a range.
+    metric: a function for the metric used to evaluate the inputs
+    ## Returns
+    a `Series` containing the metric scores at each max_depth
+    '''
     ret_lst = {}
     for depth in range(max_depth[0], max_depth[1], max_depth[2]):
         model = RandomForestClassifier(max_depth=depth, random_state=27)
@@ -44,6 +55,24 @@ def tune_random_forest(features: pd.DataFrame, target: pd.Series,
                        max_depth: Tuple[int, int, int] = (2, 31, 1),
                        min_samples_leaf: Tuple[int, int, int] = (2, 31, 1),
                        metric: callable = accuracy_score) -> pd.Series:
+    '''
+    Runs multiple versions of RandomForestClassifier
+    with different hyperparameters to determine the most effective
+    parameters for modeling.
+    ## Parameters
+    features: `DataFrame` of features to model on
+    target: `Series` of target variable
+    max_depth: a tuple representing, in order, the minimum value for max_depth,
+    the maximum value for max_depth, and the number of steps between.
+    These function as parameters for a range
+    min_samples_leaf: a tuple of integers representing, in order,
+    the minimum value for min_samples_leaf,
+    the maximum value for min_samples_leaf, and the number of steps between.
+    These are used as parameters for a range
+    metric: a function for the metric used to evaluate the inputs
+    ## Returns
+    a `DataFrame` with the metric scores at each max_depth and min_samples_leaf
+    '''
     ret_ser = {}
     for depth in range(max_depth[0], max_depth[1], max_depth[2]):
         ret_sub_ser = {}
