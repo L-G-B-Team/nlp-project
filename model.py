03398f1aa3bb06fe3542ac5cd3d6a8581123093a
Woody Sims <sims.woodrow@gmail.com>
2023-01-15 13:23:12 -0600

    modeling done

--- model.py
+++ model.py
@@ -2,15 +2,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from typing import Union, Tuple
+from typing import Union, Tuple, Dict
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.exceptions import NotFittedError
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import ConfusionMatrixDisplay
 
 ModelType = Union[LogisticRegression, DecisionTreeClassifier,
                   RandomForestClassifier, KNeighborsClassifier,
@@ -31,7 +31,7 @@ def model_data(model: ModelType, features: pd.DataFrame, target: Union[pd.Series
         y_hat = pd.Series(model.predict(features))
     y_hat.name = (result_suffix if len(result_suffix) > 0 else '')
     # changes indexes to match that of target
-    y_hat.index = target.index
+    y_hat.index = features.index 
     return y_hat
 
 
@@ -103,9 +103,9 @@ def tune_random_forest(
             model = RandomForestClassifier(
                 n_estimators=180, max_depth=depth, min_samples_leaf=leaf)
             train_yhat = model_data(model, train_features, train_target,
-                                    result_suffix='min_samples_leaf_' + str(leaf))
+                                    result_suffix=str(leaf))
             valid_yhat = model_data(model, valid_features, valid_target,
-                                    result_suffix='min_samples_leaf_' + str(leaf))
+                                    result_suffix=str(leaf))
             train_sub_ser[train_yhat.name] = metric(train_target.to_numpy(),
                                                     train_yhat.to_numpy())
             valid_sub_ser[valid_yhat.name] = metric(valid_target.to_numpy(),
@@ -148,12 +148,10 @@ def encode_for_model(train, validate, test):
     '''
     Takes in train, validate, test and adds the added features for all of them
     '''
-    train = encode_has_language(train)
-    validate = encode_has_language(validate)
-    test = encode_has_language(test)
+    ret_train = encode_has_language(train)
+    ret_validate = encode_has_language(validate)
+    ret_test = encode_has_language(test)
     return train, validate, test
-
-# TODO XG BOOST
 
 
 def tune_gradient_boost(
@@ -205,49 +203,97 @@ def tune_gradient_boost(
             pd.DataFrame(valid_ser))
 
 
-def assess_model_performance(train: pd.DataFrame, validate: pd.DataFrame) -> plt.Axes:
-    # TODO doctring
-
+def get_features_and_target(train: pd.DataFrame, validate: pd.DataFrame, test: pd.DataFrame) -> Tuple[pd.DataFrame, pd.Series, pd.DataFrame, pd.Series, pd.DataFrame, pd.Series]:
     # encode_has_language
     train_x = encode_has_language(train)
     valid_x = encode_has_language(validate)
+    test_x = encode_has_language(test)
     # scale lemmatized_len
     scaler = MinMaxScaler()
     scaled_valid = scale(validate.lemmatized_len, scaler)
     scaled_train = scale(train.lemmatized_len, scaler)
+    scaled_test = scale(test.lemmatized_len, scaler)
     # concat scaled_train and encoded_has_language
     train_x = pd.concat([scaled_train, train_x], axis=1)
     train_y = train.language
     valid_x = pd.concat([scaled_valid, valid_x], axis=1)
     valid_y = validate.language
-    rf_train, rf_valid = tune_random_forest(
+    test_x = pd.concat([scaled_test, test_x], axis=1)
+    test_y = test.language
+    return train_x, train_y, valid_x, valid_y,test_x,test_y
+
+
+def tune_hypers(train: pd.DataFrame, validate: pd.DataFrame) -> plt.Axes:
+    # TODO doctring
+    # get X and y values:
+    train_x, train_y, valid_x, valid_y = get_features_and_target(
+        train, validate)
+    return_dct = {}
+    return_dct['rf_train'], return_dct['rf_valid'] = tune_random_forest(
         train_x, train_y, valid_x, valid_y, max_depth=(2, 31, 1))
-    xg_train, xg_valid = tune_gradient_boost(
+    return_dct['xg_train'], return_dct['xg_valid'] = tune_gradient_boost(
         train_x, train_y, valid_x, valid_y)
-    dt_train, dt_valid = tune_decision_tree(train_x, train_y, valid_x, valid_y)
-    fig, axs = plt.subplots(3, 2, sharex='col')
-    sns.barplot(data=dt_train, ax=axs[0][0])
+    return_dct['dt_train'], return_dct['dt_valid'] = tune_decision_tree(
+        train_x, train_y, valid_x, valid_y)
+    return return_dct
+
+
+def model_and_evaluate(features: pd.DataFrame, target: pd.Series, model: ModelType) -> pd.DataFrame:
+    yhat = model_data(model, features, target)
+    return accuracy_score(target, yhat)
+
+
+def compare_models(train_x: pd.DataFrame, train_y: pd.Series, valid_x: pd.DataFrame, valid_y: pd.Series, decision_tree: DecisionTreeClassifier,
+                   random_forest: RandomForestClassifier, xg_boost: GradientBoostingClassifier) -> pd.DataFrame:
+    # TODO Docstring
+    ret_dict = {}
+    sub_dct = {}
+    sub_dct['Train'] = model_and_evaluate(train_x, train_y, decision_tree)
+    sub_dct['Validate'] = model_and_evaluate(valid_x, valid_y, decision_tree)
+    ret_dict['Decision Tree'] = sub_dct.copy()
+    sub_dct['Train'] = model_and_evaluate(train_x, train_y, random_forest)
+    sub_dct['Validate'] = model_and_evaluate(valid_x, valid_y, random_forest)
+    ret_dict['Random Forest'] = sub_dct.copy()
+    sub_dct['Train'] = model_and_evaluate(train_x, train_y, xg_boost)
+    sub_dct['Validate'] = model_and_evaluate(valid_x, valid_y, xg_boost)
+    ret_dict['Gradient Boosting'] = sub_dct
+    return pd.DataFrame(ret_dict)
+
+
+def plot_data(data: Dict[str, pd.DataFrame]) -> None:
+    # TODO Docstring
+    vmax = 1.0
+    vmin = .3
+    fig, axs = plt.subplots(3, 2, figsize=(10, 30), sharex='col')
+    axs[0, 0].bar(data['dt_train'].index, data['dt_train'].values)
     axs[0][0].set_xlabel('max_depth')
     axs[0][0].set_ylabel('Accuracy')
     axs[0, 0].set_title('In Sample Data')
-    sns.barplot(data=dt_valid, ax=axs[0][1])
+    axs[0, 1].bar(data['dt_valid'].index, data['dt_valid'].values)
     axs[0][1].set_xlabel('max_depth')
     axs[0][1].set_ylabel('Accuracy')
     axs[0, 1].set_title('Out of Sample Data')
-    sns.heatmap(rf_train, ax=axs[1][0])
+    sns.heatmap(data['rf_train'], ax=axs[1][0], vmin=vmin, vmax=vmax)
     axs[1, 0].set_xlabel('max_depth')
     axs[1, 0].set_ylabel('min_samples_leaf')
     axs[1, 0].set_title('In Sample Data')
-    sns.heatmap(rf_valid, ax=axs[1, 1])
+    sns.heatmap(data['rf_valid'], ax=axs[1, 1], vmin=vmin, vmax=vmax)
     axs[1, 1].set_xlabel('max_depth')
     axs[1, 1].set_ylabel('min_samples_leaf')
     axs[1, 1].set_title('Out of Sample Data')
-    sns.heatmap(xg_train, ax=axs[2, 0])
+    sns.heatmap(data['xg_train'], ax=axs[2, 0], vmin=vmin, vmax=vmax)
     axs[2, 0].set_xlabel('max_depth')
     axs[2, 0].set_ylabel('min_samples_leaf')
     axs[2, 0].set_title('In Sample Data')
-    sns.heatmap(xg_valid, ax=axs[2, 1])
+    sns.heatmap(data['xg_valid'], ax=axs[2, 1], vmin=vmin, vmax=vmax)
     axs[2, 1].set_xlabel('max_depth')
     axs[2, 1].set_ylabel('min_samples_leaf')
     axs[2, 1].set_title('Out of Sample Data')
+    plt.tight_layout()
     plt.show()
+
+def run_test(test_x:pd.DataFrame,test_y:pd.Series,model:ModelType)->ConfusionMatrixDisplay:
+    # TODO Docstring
+    yhat_test = model_data(model,test_x)
+    plt.tight_layout()
+    return ConfusionMatrixDisplay.from_predictions(test_y,yhat_test,xticks_rotation='vertical')
